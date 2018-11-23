61f365a0e8a5e91b06074d1f32d0df2901f0ad1d
blissolee <1446327445@qq.com>
2018-11-23 19:04:59 +0800

    一期截稿

--- src/main/java/com/emigroup/web/config/MyWebConfigurer.java
+++ src/main/java/com/emigroup/web/config/MyWebConfigurer.java
@@ -4,10 +4,12 @@ package com.emigroup.web.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.io.File;
@@ -17,11 +19,14 @@ public class MyWebConfigurer extends WebMvcConfigurerAdapter {
     @Autowired
     private Environment env;
 
+
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {//文件映射，访问images/的时候映射到
 //        File resourcefile = new File("src/main/resources/static");
         File resourcefile = new File("./emi_resource");
         String projectRealPath = resourcefile.getAbsolutePath();
+
 
         registry.addResourceHandler("/images/**").addResourceLocations("file:" + projectRealPath + "/");
         super.addResourceHandlers(registry);

b0cc632a183a5e77450398337d7c00b6560c4d63
blissolee <1446327445@qq.com>
2018-12-12 22:56:36 +0800

    security 改为拦截器

--- src/main/java/com/emigroup/web/config/MyWebConfigurer.java
+++ src/main/java/com/emigroup/web/config/MyWebConfigurer.java
@@ -7,10 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.util.AntPathMatcher;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
 import java.io.File;
 
@@ -37,5 +34,15 @@ public class MyWebConfigurer extends WebMvcConfigurerAdapter {
         matcher.setCaseSensitive(false);
         configurer.setPathMatcher(matcher);
     }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //拦截规则：除了index，其他都拦截判断
+        // addPathPatterns 用于添加拦截规则
+        // excludePathPatterns 用户排除拦截
+        registry.addInterceptor(new TestInterceptor()).addPathPatterns("/*/add*","/*/update*","/*/delete*");
+    }
+
+
+
 
 }
